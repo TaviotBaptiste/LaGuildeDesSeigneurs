8a1a392417698c2f24e22075158591298ca6889d
Baptiste TAVIOT <baptiste.taviot@inventhys.com>
2022-02-14 14:27:08 +0100

    V0.8.2 Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Character;
+use App\Entity\Player;
 use DateTime;
 
 class AppFixtures extends Fixture
@@ -26,6 +27,18 @@ class AppFixtures extends Fixture
                 ->setCreation(new DateTime());
             $manager->persist($character);
         }
+    for ($i = 0; $i < 10; $i++) {
+        $player = new Player();
+        $player
+            ->setFirstname("Nicolas" .$i)
+            ->setLastname("Parrot")
+            ->setEmail("NicoNikoNi@gmail.com")
+            ->setMirian(120)
+            ->setCreation(new \DateTime())
+            ->setModification((new \DateTime()))
+            ->setIdentifier(hash("sha1",uniqid()));
+        $manager->persist($player);
+    }
     $manager->flush();
     }
 }
